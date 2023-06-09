4b20296fd87e498571da50019cc3c9c41d000323
sshsrio <sourav.saha@springrain.io>
2023-06-09 16:15:59 +0600

    changed routes

--- route.js
+++ route.js
@@ -23,20 +23,20 @@ const {
 
 const routes = new Router();
 
-routes.get("/", (ctx) => {
+routes.get("/check", (ctx) => {
   ctx.body = { message: "All good" };
 });
 
 // Organization
-routes.post("/org", createOrganization);
-routes.get("/org", getAllOrg);
+routes.post("/org/create", createOrganization);
+routes.get("/orgs", getAllOrg);
 routes.get("/org/:orgId", getOrgDetails);
 routes.delete("/org/:orgId", removeOrg);
 routes.put("/org/:orgId", updateOrg);
 
 // Employee
-routes.get("/employee", getAllEmployees);
-routes.post("/employee", createEmployee);
+routes.get("/employees", getAllEmployees);
+routes.post("/employee/create", createEmployee);
 routes.get("/employee/:empId", getEmployeeDetails);
 routes.put("/employee/:empId", updateEmployeeDetails);
 routes.delete("/employee/:empId", deleteEmployee);
